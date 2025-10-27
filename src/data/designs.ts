fe98a44f5cf5191acfddeacca60021bd56f2c423
ahmedaa0378-dot <ahmed.aa0378@gmail.com>
2025-10-26 20:56:46 -0400

    Updated designs.ts

--- src/data/designs.ts
+++ src/data/designs.ts
@@ -2,57 +2,74 @@ export interface Design {
   id: string;
   name: string;
   thumbnailSrc: string;
+  fullSrc: string;
 }
 
 export const designs: Design[] = [
   {
-    id: 'pumpkin-king',
-    name: 'Pumpkin King',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1509557965875-b88c97052f0e?w=400&h=400&fit=crop'
+    id: 'ghost-ramen',
+    name: 'Ghost Ramen',
+    thumbnailSrc: '/designs/ghost-ramen.jpg',
+    fullSrc: '/designs/ghost-ramen.jpg',
   },
   {
-    id: 'haunted-house',
-    name: 'Haunted House',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1572346634942-5daa4e6e2f40?w=400&h=400&fit=crop'
+    id: 'ghost-skateboard',
+    name: 'Skateboarding Ghost',
+    thumbnailSrc: '/designs/ghost-skateboard.jpg',
+    fullSrc: '/designs/ghost-skateboard.jpg',
   },
   {
-    id: 'spooky-skull',
-    name: 'Spooky Skull',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1635776062127-d379bfcba9f8?w=400&h=400&fit=crop'
+    id: 'ghost-cauldron',
+    name: 'Cauldron Ghost',
+    thumbnailSrc: '/designs/ghost-cauldron.jpg',
+    fullSrc: '/designs/ghost-cauldron.jpg',
   },
   {
-    id: 'witch-silhouette',
-    name: 'Witch Silhouette',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1603048588665-791ca8aea617?w=400&h=400&fit=crop'
+    id: 'grim-reaper',
+    name: 'Grim Reaper',
+    thumbnailSrc: '/designs/grim-reaper.jpg',
+    fullSrc: '/designs/grim-reaper.jpg',
   },
   {
-    id: 'ghost-town',
-    name: 'Ghost Town',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1570662257892-cd6ff0d7e0b8?w=400&h=400&fit=crop'
+    id: 'scarecrow',
+    name: 'Spooky Scarecrow',
+    thumbnailSrc: '/designs/scarecrow.jpg',
+    fullSrc: '/designs/scarecrow.jpg',
   },
   {
-    id: 'bat-swarm',
-    name: 'Bat Swarm',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1508280756091-9bdd7ef1f463?w=400&h=400&fit=crop'
+    id: 'ghost-beanie',
+    name: 'Beanie Ghost',
+    thumbnailSrc: '/designs/ghost-beanie.jpg',
+    fullSrc: '/designs/ghost-beanie.jpg',
   },
   {
-    id: 'moon-night',
-    name: 'Moonlit Night',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1446292267125-fecb4ecbf1a5?w=400&h=400&fit=crop'
+    id: 'halloween-trio',
+    name: 'Halloween Trio',
+    thumbnailSrc: '/designs/halloween-trio.jpg',
+    fullSrc: '/designs/halloween-trio.jpg',
   },
   {
-    id: 'candy-corn',
-    name: 'Candy Corn',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1603344204980-4edb0ea63148?w=400&h=400&fit=crop'
+    id: 'witch-ghost',
+    name: 'Witch Ghost',
+    thumbnailSrc: '/designs/witch-ghost.jpg',
+    fullSrc: '/designs/witch-ghost.jpg',
   },
   {
-    id: 'creepy-forest',
-    name: 'Creepy Forest',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1509824227185-9c5a01ceba0d?w=400&h=400&fit=crop'
+    id: 'ghost-cat-pumpkin',
+    name: 'Ghost & Cat',
+    thumbnailSrc: '/designs/ghost-cat-pumpkin.jpg',
+    fullSrc: '/designs/ghost-cat-pumpkin.jpg',
   },
   {
-    id: 'trick-or-treat',
-    name: 'Trick or Treat',
-    thumbnailSrc: 'https://images.unsplash.com/photo-1541533848490-bc8115cd6522?w=400&h=400&fit=crop'
-  }
+    id: 'pumpkin-baby',
+    name: 'Pumpkin Baby',
+    thumbnailSrc: '/designs/pumpkin-baby.jpg',
+    fullSrc: '/designs/pumpkin-baby.jpg',
+  },
+  {
+    id: 'ghost-pumpkin-latte',
+    name: 'Pumpkin Spice Ghost',
+    thumbnailSrc: '/designs/ghost-pumpkin-latte.jpg',
+    fullSrc: '/designs/ghost-pumpkin-latte.jpg',
+  },
 ];
