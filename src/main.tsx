02769330d911f93eeefd25aeae5922a9a9e2ed15
ahmedaa0378-dot <ahmed.aa0378@gmail.com>
2025-11-04 04:19:44 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import { OrderSuccess } from './pages/OrderSuccess';
 import { ThemeProvider } from './context/ThemeContext';
+import { CartProvider } from './context/CartContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/order-success" element={<OrderSuccess />} />
-        </Routes>
-      </BrowserRouter>
+      <CartProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/order-success" element={<OrderSuccess />} />
+          </Routes>
+        </BrowserRouter>
+      </CartProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
