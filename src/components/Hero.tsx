0555dba2206f0f5493d135932fba2c2dbcfbec47
ahmedaa0378-dot <ahmed.aa0378@gmail.com>
2025-11-04 02:36:05 -0500

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export function Hero() {
        'https://i.pinimg.com/1200x/18/07/8e/18078eede2dcac62a5c38136a09f8ff5.jpg',
   'https://i.pinimg.com/736x/61/63/e4/6163e40fddce19517288b4b3757a7e5b.jpg',
     'https://i.pinimg.com/1200x/74/3d/56/743d56ada745492a776a73c0423d8442.jpg',
-    'https://i.pinimg.com/1200x/2b/60/cd/2b60cd42a7d163a23cda0ac2fa076ce6.jpg',
+  'https://i.pinimg.com/736x/6b/61/c3/6b61c3d9ddc2ead7fb9cb1d83e4e15f2.jpg',
     'https://i.pinimg.com/1200x/64/07/86/64078626f2744ec46a9c7c08fbec2601.jpg',
     'https://i.pinimg.com/1200x/00/c7/28/00c72863fc8ca97ed285add5dc615bd5.jpg',
 
